4f00b715f3e74293bdbe0f0196787da8000ae36f
PeterKlu <pvknyc@gmail.com>
2026-03-19 19:15:11 -0400

    Made enrollment commands async

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -3,11 +3,11 @@ const write = require('./fileWriteAndRespond');
 async function blacklist(whitelistedUsers, fs, interaction, filePath) {
     const INTERACTION_USER_ID = interaction?.member?.user?.id;
     if (whitelistedUsers.some(id => id === INTERACTION_USER_ID)) {
-        whitelistedUsers = whitelistedUsers.filter(value => value != INTERACTION_USER_ID);
+        whitelistedUsers = await whitelistedUsers.filter(value => value != INTERACTION_USER_ID);
         const json = JSON.stringify(whitelistedUsers, 'whitelisted-ids', 2);
         await write(fs, filePath, json, `Successfully opted out <@${INTERACTION_USER_ID}>`, interaction);
     } else {
-        await interaction
+        interaction
             .reply({ content: "You aren't among users who have opted in", ephemeral: true })
             .catch((error) => console.log(error.message));
     }
--- commands/whitelist.js
+++ commands/whitelist.js
@@ -1,13 +1,13 @@
 const write = require('./fileWriteAndRespond');
 
-async function whitelist(blacklistedUsers, fs, interaction, filePath) {
+async function whitelist(whitelistedUsers, fs, interaction, filePath) {
     const INTERACTION_USER_ID = interaction?.user?.id;
-    if (!blacklistedUsers.some(id => id === INTERACTION_USER_ID)) {
-        blacklistedUsers.push(INTERACTION_USER_ID.toString());
-        const json = JSON.stringify(blacklistedUsers, 'whitelisted-ids', 2);
+    if (!whitelistedUsers.some(id => id === INTERACTION_USER_ID)) {
+        await whitelistedUsers.push(INTERACTION_USER_ID.toString());
+        const json = JSON.stringify(whitelistedUsers, 'whitelisted-ids', 2);
         await write(fs, filePath, json, `Successfully opted in <@${INTERACTION_USER_ID}>`, interaction);
     } else {
-        await interaction
+        interaction
             .reply({ content: "You've already opted in", ephemeral: true })
             .catch((error) => console.log(error.message));
     }
